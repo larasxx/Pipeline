65139ec8298764e131ee01e16ba72db1d7955bfa
P-S-BRAINCUBE\skpav <pavlos.skiadas@braincube.gr>
2025-02-10 21:54:43 +0200

    Pipeline testing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p></p>
+        <a className="App-link" target="_blank" rel="noopener noreferrer">
+          Hello World!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
